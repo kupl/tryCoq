b2c08f97c0c58b8b3bf8240bc4f8f9e532c1af4f
jmkjmt <mingyu_jo@DESKTOP-5OVFE5C.>
2025-01-22 13:07:21 +0900

    tactic skeleton

--- lib/proof.ml
+++ lib/proof.ml
@@ -1,7 +1,7 @@
 open Sexplib.Std
 
 type t = (fact list * goal) list [@@deriving sexp]
-and fact = prop [@@deriving sexp]
+and fact = string * prop [@@deriving sexp]
 and goal = prop [@@deriving sexp]
 
 and prop =
@@ -11,9 +11,9 @@ and prop =
   | And of prop * prop
   | Or of prop * prop
   | Not of prop
-  | Forall of string * prop
+  | Forall of string * prop * prop
   | Imply of prop * prop
-  | Type of string * string
+  | Type of string
 [@@deriving sexp]
 
 and expr = Ir.expr [@@deriving sexp]
@@ -21,7 +21,6 @@ and expr = Ir.expr [@@deriving sexp]
 type theorem = tactic list * goal [@@deriving sexp]
 
 and tactic =
-  | ApplyInAt of string * string * int
   | Intro of string
   | RewriteInAt of string * string * int
   | Induction of string
@@ -30,52 +29,79 @@ and tactic =
   | Reflexivity
 [@@deriving sexp]
 
-let get_type env name =
-  ignore (env, name);
-  "int"
+type env = Ir.t [@@deriving sexp]
+
+let apply_intro name facts goal =
+  let rec search_qvar name goal : goal =
+    match goal with
+    | Forall (var, typ, goal') ->
+      if var = name then goal' else Forall (var, typ, search_qvar name goal')
+    | _ -> failwith "There is no such universal variable in goal"
+  in
+  match goal with
+  | Forall (var, typ, goal) ->
+    if var = name
+    then facts @ [ name, typ ], goal
+    else facts @ [ name, typ ], Forall (var, typ, search_qvar name goal)
+  | Imply (p1, p2) -> facts @ [ name, p1 ], p2
+  | _ -> failwith "There is no term that can be introduced"
 ;;
 
-let rec search_qvar name goal : goal =
+let apply_eq goal =
+  let rec peel_forall goal =
+    match goal with
+    | Forall (_, _, goal') -> peel_forall goal'
+    | Eq (e1, e2) -> if e1 = e2 then [] else failwith "LHS and RHS are not equal"
+    | _ -> failwith "The goal is not an equality"
+  in
   match goal with
-  | Forall (var, goal') ->
-    if var = name then goal' else Forall (var, search_qvar name goal')
-  | _ -> failwith "There is no such universal variable in goal"
+  | Eq (e1, e2) -> if e1 = e2 then [] else failwith "LHS and RHS are not equal"
+  | Forall (_, _, goal) -> peel_forall goal
+  | _ -> failwith "The goal is not an equality"
+;;
+
+let apply_induction name facts goal =
+  ignore name;
+  ignore facts;
+  ignore goal;
+  failwith "Not implemented"
+;;
+
+let apply_rewrite facts goal fact target i =
+  ignore facts;
+  ignore goal;
+  ignore fact;
+  ignore target;
+  ignore i;
+  failwith "Not implemented"
+;;
+
+let apply_case name facts goal =
+  ignore name;
+  ignore facts;
+  ignore goal;
+  failwith "Not implemented"
+;;
+
+let apply_simpl facts goal fact target i =
+  ignore facts;
+  ignore goal;
+  ignore fact;
+  ignore target;
+  ignore i;
+  failwith "Not implemented"
 ;;
 
 let apply_tactic t env tactic : t =
+  ignore env;
   let facts, goal = List.hd t in
   match tactic with
-  | ApplyInAt (fact, target, i) ->
-    ignore (fact, target, i);
-    t
-  | Intro name ->
-    let new_state =
-      match goal with
-      | Forall (var, goal) ->
-        if var = name
-        then facts @ [ Type (name, get_type env name) ], goal
-        else
-          facts @ [ Type (name, get_type env name) ], Forall (var, search_qvar name goal)
-      | Imply (p1, p2) -> facts @ [ p1 ], p2
-      | _ -> failwith "There is no term that can be introduced"
-    in
-    new_state :: List.tl t
-  | RewriteInAt (fact, target, i) ->
-    ignore (fact, target, i);
-    t
-  | Induction name ->
-    ignore name;
-    t
-  | Case name ->
-    ignore name;
-    t
-  | SimplInAt (fact, target, i) ->
-    ignore (fact, target, i);
-    t
-  | Reflexivity ->
-    (match goal with
-     | Eq (e1, e2) when e1 = e2 -> List.tl t
-     | _ -> failwith "The goal is not an equality")
+  | Intro name -> apply_intro name facts goal :: List.tl t
+  | RewriteInAt (fact, target, i) -> apply_rewrite facts goal fact target i @ List.tl t
+  | Induction name -> apply_induction name facts goal @ List.tl t
+  | Case name -> apply_case name facts goal @ List.tl t
+  | SimplInAt (fact, target, i) -> apply_simpl facts goal fact target i :: List.tl t
+  | Reflexivity -> apply_eq goal @ List.tl t
 ;;
 
 let string_of_t t = t |> sexp_of_t |> Sexplib.Sexp.to_string
@@ -93,15 +119,15 @@ let rec pp_prop prop =
   | And (p1, p2) -> pp_prop p1 ^ " /\\ " ^ pp_prop p2
   | Or (p1, p2) -> pp_prop p1 ^ " \\/ " ^ pp_prop p2
   | Not p -> "not " ^ pp_prop p
-  | Forall (name, p) -> "forall " ^ name ^ ". " ^ pp_prop p
+  | Forall (name, typ, p) -> "forall " ^ name ^ ":" ^ pp_prop typ ^ ". " ^ pp_prop p
   | Imply (p1, p2) -> pp_prop p1 ^ " -> " ^ pp_prop p2
-  | Type (name, typ) -> name ^ " : " ^ typ
+  | Type typ -> typ
 ;;
+
+let pp_fact (name, prop) = name ^ " : " ^ pp_prop prop
 
 let pp_tactic tactic =
   match tactic with
-  | ApplyInAt (fact, goal, i) ->
-    "apply " ^ fact ^ " in " ^ goal ^ " at " ^ string_of_int i
   | Intro name -> "intro " ^ name
   | RewriteInAt (fact, goal, i) ->
     "rewrite " ^ fact ^ " in " ^ goal ^ " at " ^ string_of_int i
@@ -119,7 +145,7 @@ let pp_theorem (tactics, goal) =
 let pp_t (t : t) =
   List.map
     (fun (facts, goal) ->
-       (List.map pp_prop facts |> String.concat "\n")
+       (List.map pp_fact facts |> String.concat "\n")
        ^ "\n---------------------------------------\n"
        ^ pp_prop goal)
     t
@@ -128,21 +154,12 @@ let pp_t (t : t) =
 
 let mk_proof program_a program_b func_name =
   (* dummy *)
+  let env = program_a @ program_b in
+  ignore func_name;
   ignore program_a;
   ignore program_b;
-  let goal =
-    Forall
-      ( "n1"
-      , Forall
-          ( "n2"
-          , Eq
-              ( Ir.Call (func_name ^ "_ta1", [ Ir.Var "n1"; Ir.Var "n2" ])
-              , Ir.Call (func_name ^ "_ta2", [ Ir.Var "n1"; Ir.Var "n2" ]) ) ) )
-  in
-  List.fold_left
-    (fun t tactic -> apply_tactic t [] tactic)
-    [ [], goal ]
-    [ Intro "n1"; Intro "n2" ]
+  let goal = Forall ("x", Type "nat", Forall ("y", Type "nat", Eq (Var "x", Var "x"))) in
+  List.fold_left (fun t tactic -> apply_tactic t env tactic) [ [], goal ] [ Reflexivity ]
   |> pp_t
   |> print_endline
 ;;
