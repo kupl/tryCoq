a86edc8b878315d18cb41f928cfc05849f4e9ec7
jmkjmt <mingyu_jo@DESKTOP-5OVFE5C.>
2025-02-13 13:01:02 +0900

    update

--- lib/engine.ml
+++ lib/engine.ml
@@ -5,7 +5,5 @@ let proof_top std_lib program_a program_b =
   let std_lib = std_lib |> Ir.t_of_typedtree in
   let program_a = program_a |> Ir.t_of_typedtree in
   let program_b = program_b |> Ir.t_of_typedtree in
-  let _ = Ir.string_of_t program_a |> print_endline in
-  let _ = Ir.string_of_t program_b |> print_endline in
   Proof.proof_top (std_lib @ program_a @ program_b)
 ;;
