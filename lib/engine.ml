20be3f5053493d158b2381c92686ef0cf2bbe58a
jmkjmt <mingyu_jo@DESKTOP-5OVFE5C.>
2025-02-14 16:59:41 +0900

    synthesis skeleton

--- lib/engine.ml
+++ lib/engine.ml
@@ -5,5 +5,14 @@ let proof_top std_lib program_a program_b =
   let std_lib = std_lib |> Ir.t_of_typedtree in
   let program_a = program_a |> Ir.t_of_typedtree in
   let program_b = program_b |> Ir.t_of_typedtree in
-  Proof.proof_top (std_lib @ program_a @ program_b)
+  let _ = program_a |> Ir.pp_t |> print_endline in
+  let env = std_lib @ program_a @ program_b in
+  let goal = "assert forall (a:nat) (b:nat), natadd_ta1 a b = natadd_ta2 a b" in
+  let goal = Proof.parse_tactic Proof.empty_t goal env in
+  let result = Prover.progress env [ Proof.empty_t, goal, 0 ] [] in
+  match result with
+  | _, [] -> print_endline "Proof Success"
+  | _, _ ->
+    print_endline "Proof Failed";
+    Proof.pp_t result |> print_endline
 ;;
