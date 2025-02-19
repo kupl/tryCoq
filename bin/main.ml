f110f6ad3052e05f92fce8eeaf30dc6c4375388f
jmkjmt <mingyu_jo@DESKTOP-5OVFE5C.>
2025-02-19 19:17:24 +0900

    update

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,7 @@
 let main =
   let std_lib = "primitive_type/std_lib.ml" in
   let program_a = "../dilemma-benchmark/dilemma-bench/dilemma/natmul/ta1.ml" in
-  let program_b = "../dilemma-benchmark/dilemma-bench/dilemma/natmul/sol32.ml" in
+  let program_b = "../dilemma-benchmark/dilemma-bench/dilemma/natmul/sol116.ml" in
   let std_lib = Dilemma.Utils.File.read_file_from_path std_lib in
   let program_a = Dilemma.Utils.File.read_file_from_path program_a in
   let program_b = Dilemma.Utils.File.read_file_from_path program_b in
