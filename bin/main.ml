fc7112fcc24a7dd90e24d414504ceccef44e61a2
jmkjmt <mingyu_jo@korea.ac.kr>
2025-09-09 07:12:16 +0000

    renaming

--- bin/main.ml
+++ bin/main.ml
@@ -7,21 +7,21 @@ let main =
   let _ = print_endline "Enter the definition file path (2/2) : " in
   let _ = print_string "> " in
   let program_b = read_line () in
-  let definition = Dilemma.Utils.File.read_file_from_path definition in
-  let axiom = Dilemma.Utils.File.read_file_from_path axiom in
-  let program_a = Dilemma.Utils.File.read_file_from_path program_a in
-  let program_b = Dilemma.Utils.File.read_file_from_path program_b in
+  let definition = TryCoq.Utils.File.read_file_from_path definition in
+  let axiom = TryCoq.Utils.File.read_file_from_path axiom in
+  let program_a = TryCoq.Utils.File.read_file_from_path program_a in
+  let program_b = TryCoq.Utils.File.read_file_from_path program_b in
   let _ = print_endline "Choose the proof type :" in
   let _ = print_endline "1) Interactive Mode \t 2) Auto Mode" in
   let s = read_line () in
   if s = "1"
   then (
-    let _ = Dilemma.Engine.proof_top definition axiom program_a program_b in
+    let _ = TryCoq.Engine.proof_top definition axiom program_a program_b in
     ())
   else (
     let _ = print_string "Enter the goal : " in
     let goal = read_line () in
-    let _ = Dilemma.Engine.proof_auto definition axiom program_a program_b goal in
+    let _ = TryCoq.Engine.proof_auto definition axiom program_a program_b goal in
     ())
 ;;
 
