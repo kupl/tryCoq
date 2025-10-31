36ee409f0ec14c1ac37aedeb9a99e8a17e60e9f2
jmkjmt <mingyu_jo@korea.ac.kr>
2025-10-31 14:50:10 +0900

    licensed annotate

--- vendor/ego/lib/generic.mli
+++ vendor/ego/lib/generic.mli
@@ -1,3 +1,11 @@
+(*
+  This file includes modified code from the Ego project
+  (https://github.com/verse-lab/ego),
+  which is licensed under the GNU General Public License v3.0.
+
+  Modifications © 2025 Migyu Jo.
+  See LICENSE.GPL for full license text.
+*)
 open Language
 
 type ('node, 'data) eclass = {
